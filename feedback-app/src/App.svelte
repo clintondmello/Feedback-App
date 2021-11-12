0218943870534ad4237fea8d35a4b120a2ed5a6e
Clinton Dmello <clinton.dmello95@gmail.com>
2021-11-12 22:42:52 +0530

    Added Feedback Page

---
+++ feedback-app/src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import FeedbackList from "./Components/FeedbackList.svelte";
+  import FeedbackStats from "./Components/FeedbackStats.svelte";
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+  ];
+
+  //Event Forwarding
+  const deleteFeedback = (e) => {
+    const itemId = e.detail;
+    feedback = feedback.filter((item) => item.id != itemId);
+  };
+
+  $: count = feedback.length;
+  $: average =
+    feedback.reduce((a, { rating }) => a + rating, 0) / feedback.length;
+</script>
+
+<main class="container">
+  <FeedbackStats {count} {average} />
+  <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
+</main>
---
+++ feedback-app/src/Components/FeedbackList.svelte
@@ -0,0 +1,9 @@
+<script>
+  import FeedbackItem from "./FeedbackItem.svelte";
+  export let feedback = []; //If not passed in should be an empty array
+  console.log(feedback);
+</script>
+
+{#each feedback as fb (fb.id)}
+  <FeedbackItem item={fb} on:delete-feedback />
+{/each}
